c68150f5df1284724835de35ca4f95809f18d982
Randall Sexton <jrandallsexton@gmail.com>
2015-05-12 15:42:22 -0500

    addition of test VS solution and simple class

---
+++ src/gitflow-test/gitflow-test.core/SimpleClass.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace gitflow_test.core
+{
+    public class SimpleClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public void DoFoo()
+        {
+            Console.WriteLine("Doing foo");
+        }
+    }
+}
