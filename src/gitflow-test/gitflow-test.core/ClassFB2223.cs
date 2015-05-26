061b38591796dadfdd4aaffa90f1e133de37ba1e
Randall Sexton <jrandallsexton@gmail.com>
2015-05-26 17:32:30 -0500

    #FB2223 - added class

---
+++ src/gitflow-test/gitflow-test.core/ClassFB2223.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitflow_test.core
+{
+    public class ClassFB2223
+    {
+        public string Desc { get; set; }
+
+        public ClassFB2223()
+        {
+            this.Desc = "I was added for FB2223";
+        }
+    }
+}
